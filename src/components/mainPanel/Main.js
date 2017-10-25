17cadc08eb68be861881f887f54c6f8805beecf3
Unknown <romainchazottier@gmail.com>
2017-10-25 16:32:52 +0200

    Mise au propre archi - 4.1

--- src/components/mainPanel/Main.js
+++ src/components/mainPanel/Main.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../../logo.svg';
+import './Main.css';
 
-class App extends Component {
+class Main extends Component {
   render() {
     return (
       <div className="App">
@@ -11,11 +11,11 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          YOLOLOLO : To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
   }
 }
 
-export default App;
+export default Main;
